12d6311e6392fa756926e7afb159c2692c5fe168
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-13 21:56:09 +0800

    allow dynamic int change

--- In_C/main.c
+++ In_C/main.c
@@ -10,26 +10,28 @@ int main(){
     /* Funct*\ion Declerations */
     char *str;
     char *second;
-
+    int n, m;
+    n = 6;
+    m = 200;
     /* Initializing Global Variables */
 
     /* EXPERIMENTING WITH FGETS */
     /* Initial memory allocation */
-    str = malloc(200 * sizeof(char));
-    second = malloc(200 * sizeof(char));
+    str = malloc(n * sizeof(char));
+    second = malloc(m * sizeof(char));
     /* Get user input and print results */
     printf("Enter a string: "); // ask user to put in a string
-    fgets(str, 200, stdin);
+    fgets(str, n, stdin);
     str[strlen(str) - 1] = '\0'; // Removes new line character of fgets
     printf("Enter a another string: "); // ask ujason is a godser to put in a string
-    fgets(second, 200, stdin);
+    fgets(second, m, stdin);
     second[strlen(second) - 1] = '\0'; // Removes new line character of fgets
     printf("String = %s, Address of String is = %p\n", str, str);
     printf("String = %s, Address of String is = %p\n\n\n", second, second);
 
     
     /* Reallocating memory */
-    str = (char *)realloc(str, (100 * sizeof(char)));
+    str = (char *)realloc(str, (m * sizeof(char)));
     printf("Combine text\n");
     strcat(str, second);
     printf("String = %s, Address of String is = %p\n", str, str);
